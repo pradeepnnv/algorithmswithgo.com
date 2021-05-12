24f4a079a4298b397f927ad341f77d0784512969
pradeepnnv <pradeepnnv@gmail.com>
2021-05-11 22:47:25 -0500

    sum with recursion
    Signed-off-by: pradeepnnv <pradeepnnv@gmail.com>

--- module01/sum.go
+++ module01/sum.go
@@ -2,10 +2,16 @@ package module01
 
 // Sum will sum up all of the numbers passed
 // in and return the result
+// func Sum(numbers []int) int {
+// 	sum := 0
+// 	for _, i := range numbers {
+// 		sum += i
+// 	}
+// 	return sum
+// }
 func Sum(numbers []int) int {
-	sum := 0
-	for _, i := range numbers {
-		sum += i
+	if len(numbers) == 0 {
+		return 0
 	}
-	return sum
+	return numbers[0] + Sum(numbers[1:])
 }
