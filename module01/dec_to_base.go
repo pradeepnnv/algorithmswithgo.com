492a44e57d5c4192fc7a6450fc8b112587abbe74
pradeepnnv <pradeepnnv@gmail.com>
2021-05-17 22:08:37 -0500

    initial version
    Signed-off-by: pradeepnnv <pradeepnnv@gmail.com>

--- module01/dec_to_base.go
+++ module01/dec_to_base.go
@@ -1,5 +1,7 @@
 package module01
 
+import "fmt"
+
 // DecToBase will return a string representing
 // the provided decimal number in the provided base.
 // This is limited to bases 2-16 for simplicity.
@@ -10,5 +12,11 @@ package module01
 //   DecToBase(14, 2) => "1110"
 //
 func DecToBase(dec, base int) string {
-	return ""
+	var convValue string
+	for dec > 0 {
+		rem := dec % base
+		convValue = fmt.Sprintf("%X%s", rem, convValue)
+		dec = dec / base
+	}
+	return convValue
 }
